912e62655d4b18a7f287278c3809a7046e4832e9
kirld <kiraderksen@Kiras-MacBook-Pro.local>
2016-07-18 01:05:12 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -1,78 +1,21 @@
-    // The watch id references the current `watchAcceleration`
-    var watchID = null;
+    
+var canvasWidth = 250;
+var canvasHeight = 500;
+var centerScreen = canvasWidth / 2;
+var renderer = PIXI.autoDetectRenderer(canvasWidth, canvasHeight,{backgroundColor : 0x1099bb});
+document.body.appendChild(renderer.view);
 
-    // Wait for device API libraries to load to prevent errors
-    document.addEventListener("deviceready", onDeviceReady, false);
+// create the root of the scene graph
+var stage = new PIXI.Container();
 
-    // device APIs are available and ready so start the 
-    // accelerometer and begin checking device position/orientation
-    function onDeviceReady() {
-        startWatch();
-    }
+// create a texture from an image path
+var sealTexture = PIXI.Texture.fromImage('img/seal.png');
 
-    // Start watching the accelerometer values
-    function startWatch() {
-        // Create options object to update properties of accelerometer
-        // Update acceleration every 50 milliseconds
-        var options = { frequency: 50 };
-        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-    }
+// create a texture from an image path
+var icebergTexture = PIXI.Texture.fromImage('img/iceberg.png');
 
-    // Stop watching the acceleration
-    function stopWatch() {
-        if (watchID) {
-            navigator.accelerometer.clearWatch(watchID);
-            watchID = null;
-        }
-    }
-
-    // onSuccess: Get a snapshot of the current acceleration
-    // And display these on the page
-    function onSuccess(acceleration) {
-
-        element.innerHTML = acceleration.x;
-        var character = document.getElementById("char");
-        if(acceleration.x >= 4){
-        	character.style.right = "250px";
-        }
-        else if(acceleration.x >= 3 && acceleration.x < 4){
-        	character.style.right = "218.75px";
-        }
-    	else if(acceleration.x >= 2 && acceleration.x < 3){
-        	character.style.right = "187.5px";
-        }
-        else if(acceleration.x >= 1 && acceleration.x < 2){
-        	character.style.right = "156.25px";
-        }
-        else if(acceleration.x >= 0 && acceleration.x < 1){
-        	character.style.right = "140px";
-        }
-    	else if(acceleration.x >= -1 && acceleration.x < 0){
-        	character.style.right = "125px";
-        }
-    	else if(acceleration.x >= -2 && acceleration.x < -1){
-        	character.style.right = "93.75px";
-        }
-      	else if(acceleration.x >= -3 && acceleration.x < -2){
-        	character.style.right = "62.5px";
-        }
-        else if(acceleration.x > -4 && acceleration.x < -3){
-        	character.style.right = "31.25px";
-        }
-        else if(acceleration.x <= -4){
-       		character.style.right = "0px";
-        }
-    }
-
-    // onError: Failed to get the acceleration
-    function onError() {
-        alert('onError!');
-    }
-
-    // get a refrence to the canvas and its context
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var image = document.getElementById("iceberg");
+// create a new Sprite using the texture
+var seal = new PIXI.Sprite(sealTexture);
 
 // newly spawned objects start at Y=25
 var spawnLineY = 25;
@@ -81,89 +24,177 @@ var spawnLineY = 25;
 var spawnRate = 5000;
 
 // set how fast the objects will fall
-var spawnRateOfDescent = 1;
+var icebergSpeed = 1;
 
 // when was the last object spawned
 var lastSpawn = -1;
 
 // this array holds all spawned object
-var objects = [];
+var icebergObjects = [];
 
 // save the starting time (used to calc elapsed time)
 var startTime = Date.now();
 
-// start animating
-animate();
+
+// center the sprite's anchor point
+seal.anchor.x = 0.5;
+seal.anchor.y = 0.5;
+
+// move the sprite to the center of the screen
+seal.position.x = 200;
+seal.position.y = 150;
+
+seal.width = 25;
+seal.height = 40;
 
 
-function spawnRandomObject() {
+stage.addChild(seal);
 
-    // select a random type for this new object
-    var t;
+function createIceberg(){
+    // create a new Sprite using the texture
+    var iceberg = new PIXI.Sprite(icebergTexture);
 
-    // About Math.random()
-    // Math.random() generates a semi-random number
-    // between 0-1. So to randomly decide if the next object
-    // will be A or B, we say if the random# is 0-.49 we
-    // create A and if the random# is .50-1.00 we create B
+    var icebergObject = {
+        instance: iceberg
+    };
 
+    // center the sprite's anchor point
+    iceberg.anchor.x = 0.5;
+    iceberg.anchor.y = 0.5;
 
-    // if (Math.random() < 0.50) {
-    //     t = "red";
-    // } else {
-    //     t = "blue";
-    // }
+    // move the sprite to the center of the screen
+    iceberg.position.x = Math.random() * canvasWidth;
+    iceberg.position.y = 0;
 
-    // create the new object
-    var object = {
-        // set this objects type
-        type: t,
-        // set x randomly but at least 15px off the canvas edges
-        x: Math.random() * (canvas.width - 30) + 15,
-        // set y to start on the line where objects are spawned
-        y: spawnLineY,
+    iceberg.width = 40;
+    iceberg.height = 40;
+
+    icebergObjects.push(icebergObject);
+
+    stage.addChild(iceberg);
+
+}
+
+// The watch id references the current `watchAcceleration`
+var watchID = null;
+
+// Wait for device API libraries to load to prevent errors
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available and ready so start the 
+// accelerometer and begin checking device position/orientation
+function onDeviceReady() {
+    startWatch();
+}
+
+// Start watching the accelerometer values
+function startWatch() {
+    // Create options object to update properties of accelerometer
+    // Update acceleration every 50 milliseconds
+    var options = { frequency: 50 };
+    watchID = navigator.accelerometer.watchAcceleration(onAccelerate, onError, options);
+}
+
+// Stop watching the acceleration
+function stopWatch() {
+    if (watchID) {
+        navigator.accelerometer.clearWatch(watchID);
+        watchID = null;
     }
+}
 
-    // add the new object to the objects[] array
-    objects.push(object);
+var xPos = centerScreen;
+
+// onSuccess: Get a snapshot of the current acceleration
+// And display these on the page
+function onAccelerate(acceleration) {
+
+    element.innerHTML = acceleration.x;
+
+    var maxAngle = 4;
+    var angleRatio = acceleration.x / maxAngle;
+    xPos = angleRatio * centerScreen + centerScreen;
+}
+
+// onError: Failed to get the acceleration
+function onError() {
+    alert('onError!');
 }
 
 
 
+
+
+// function spawnRandomObject() {
+
+//     // select a random type for this new object
+//     var t;
+
+//     // About Math.random()
+//     // Math.random() generates a semi-random number
+//     // between 0-1. So to randomly decide if the next object
+//     // will be A or B, we say if the random# is 0-.49 we
+//     // create A and if the random# is .50-1.00 we create B
+
+
+//     // create the new object
+//     var object = {
+//         // set this objects type
+//         type: t,
+//         // set x randomly but at least 15px off the canvas edges
+//         x: Math.random() * (canvas.width - 30) + 15,
+//         // set y to start on the line where objects are spawned
+//         y: spawnLineY,
+//     }
+
+//     // add the new object to the objects[] array
+//     objects.push(object);
+// }
+
+// start animating
+animate();
+
 function animate() {
+    // request another animation frame
+    requestAnimationFrame(animate);
 
     // get the elapsed time
     var time = Date.now();
 
+    var sealTop = seal.y - seal.height / 2;
+    var sealLeft = seal.x - seal.width / 2;
+    var sealRight = seal.x + seal.width / 2;
+
     // see if its time to spawn a new object
     if (time > (lastSpawn + spawnRate)) {
         lastSpawn = time;
-        spawnRandomObject();
+        createIceberg();
     }
 
-    // request another animation frame
-    requestAnimationFrame(animate);
-
-    // clear the canvas so all objects can be 
-    // redrawn in new positions
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // draw the line where new objects are spawned
-    // ctx.beginPath();
-    // ctx.moveTo(0, spawnLineY);
-    // ctx.lineTo(canvas.width, spawnLineY);
-    // ctx.stroke();
 
     // move each object down the canvas
-    for (var i = 0; i < objects.length; i++) {
-        var object = objects[i];
-        object.y += spawnRateOfDescent;
-        // ctx.drawImage(image);
-        ctx.beginPath();
-        ctx.arc(object.x, object.y, 8, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fillStyle = object.type;
-        ctx.fill();
+    for (var i = 0; i < icebergObjects.length; i++) {
+        var iceberg = icebergObjects[i];
+        var icebergWidth = iceberg.instance.width;
+        var icebergHeight = iceberg.instance.height;
+        var icebergTop = iceberg.instance.y - icebergHeight / 2;
+        var icebergLeft = iceberg.instance.x - icebergWidth / 2;
+        var icebergRight = iceberg.instance.x + icebergWidth / 2;
+        var icebergBottom = iceberg.instance.y + icebergHeight / 2;
+
+        iceberg.instance.position.y+= icebergSpeed;
+
+        if(sealTop < icebergBottom && sealTop > icebergTop){
+
+            if(sealRight > icebergLeft && sealLeft < icebergRight){
+                alert("Collide");
+            }
+        }
     }
+
+    seal.position.x = xPos;
+
+    // render the container
+    renderer.render(stage);
 
 }
