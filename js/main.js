6c5aeec85d254f45d5a57ee7c13e8b778839c6d7
kirld <kiraderksen@hotmail.ca>
2016-07-20 18:34:26 -0700

    new function

--- js/main.js
+++ js/main.js
@@ -160,16 +160,16 @@ function animate() {
             //check for hit on x axis
             if(sealRight > icebergLeft && sealLeft < icebergRight){
                 //vibrate phone to alert user theyve hit iceberg
-                // navigator.notification.vibrate(500); 
-                // // alert("Game over press okay to replay")   
-                navigator.notification.alert("Oh no, you've hit an iceberg", restart, "Game Over", "Play Again!");   
-                function restart(){   
+                navigator.notification.vibrate(500); 
                     for(var i = stage.children.length - 1; i >= 0; i--) {  
                         stage.removeChild(stage.children[i]);
                     };       
                     //draw the image onto the canvas
                     stage.addChild(seal);
                     icebergObjects = [];    
+                // // alert("Game over press okay to replay")   
+                navigator.notification.alert("Oh no, you've hit an iceberg", restart, "Game Over", "Play Again!");   
+                function restart(){   
                     animate();
                 }  
             }
