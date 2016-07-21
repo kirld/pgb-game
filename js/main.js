c8c9e808c7c621dc3f6fe1b89c51579daa5f4a34
kirld <kiraderksen@hotmail.ca>
2016-07-20 18:19:59 -0700

    update alert

--- js/main.js
+++ js/main.js
@@ -165,16 +165,17 @@ function animate() {
             if(sealRight > icebergLeft && sealLeft < icebergRight){
                 //vibrate phone to alert user theyve hit iceberg
                 navigator.notification.vibrate(500); 
+                for(var i = stage.children.length - 1; i >= 0; i--) {  
+                    stage.removeChild(stage.children[i]);
+                };
+                icebergObjects = []; 
                 function restart(){             
-                    for(var i = stage.children.length - 1; i >= 0; i--) {  
-                        stage.removeChild(stage.children[i]);
-                    };
                     //draw the image onto the canvas
                     stage.addChild(seal);
-                    icebergObjects = []; 
+                    animate();
                 } 
                 // alert("Game over press okay to replay")   
-                navigator.notification.alert("Oh no, you've hit an iceber", restart, "Game Over", "Play Again")    
+                navigator.notification.alert("Oh no, you've hit an iceberg", restart, "Game Over", "Play Again")    
             }
         }
     }
